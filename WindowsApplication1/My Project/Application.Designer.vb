a970e9d6c40523fc1b5a872cb0c8c2439601fc79
laforcej <laforcej@gmail.com>
2016-11-25 15:29:39 -0500

    Add files via upload

--- WindowsApplication1/My Project/Application.Designer.vb
+++ WindowsApplication1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MKL.Form1
+            Me.MainForm = Global.MKL.Main
         End Sub
     End Class
 End Namespace
